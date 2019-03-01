122b6eb9331dbbb909fbacfb4ef1a2924e83e5cb
Dubravko <woose@web.de>
2019-03-01 13:12:29 +0100

    slide animation added

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
         <transition name="fade">
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
+        <transition name="slide">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -44,5 +47,38 @@ export default {
 .fade-leave-active {
   transition: opacity 1s;
   opacity: 0;
+}
+/**** slide ****/
+.slide-enter {
+}
+
+/** forwards = dadurch bleibt das animierte Element an der Zielposition stehen und
+               geht nicht zurück an den start. ***/
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+}
+
+.slide-leave {
+}
+.slide-leave-active {
+  animation: slide-out 1s ease-out;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
 }
 </style>
