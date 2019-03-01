80ed0248b1885edfe8a2fe7343e44d5dcf1a7400
Dubravko <woose@web.de>
2019-03-01 12:55:37 +0100

    fade fade out transistion with vue transition obj

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show=!show">Show alert</button>
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Test from "./components/Test.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      show: false
+    };
+  },
+  components: {
+    appTest: Test
+  }
+};
+</script>
+
+<style>
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 1s;
+}
+
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">Hello People!</div>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped >
+.test {
+  position: fixed;
+
+  top: 400;
+  height: 200px;
+  width: 100px;
+  background-color: yellow;
+}
+</style>
